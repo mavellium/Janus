425532661c3111ccdb27542c77c53b07517d4d6e
Luan <mavellium@gmail.com>
2026-05-21 16:15:40 -0300

    Fix: Capa blog

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
   images: {
     remotePatterns: [
       {
